40c646060f49080c9706514ef1d524f87f1c3392
heraya <herayafpm@gmail.com>
2021-07-11 17:03:07 +0700

    penambahan term sebelum booking

--- lib/controllers/booking_controller.dart
+++ lib/controllers/booking_controller.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_html/flutter_html.dart';
 import 'package:get/get.dart';
 import 'package:mount_slamet/bloc/booking/booking_bloc.dart';
+import 'package:mount_slamet/repositories/blog_repository.dart';
 import 'package:mount_slamet/utils/date_time_util.dart';
+import 'package:html/dom.dart' as dom;
+import 'package:url_launcher/url_launcher.dart';
 
 class BookingController extends GetxController {
   TextEditingController jumlahPendakiController = TextEditingController();
@@ -31,6 +35,15 @@ class BookingController extends GetxController {
           DateTimeUtil.convert(tglKeluar.value)));
   }
 
+  final persetujuan = "Persetujuan".obs;
+
+  @override
+  void onInit() async {
+    Map<String, dynamic> blog = await BlogRepository.informasi();
+    persetujuan.value = blog['data']['data']['blog_isi'] ?? "";
+    super.onInit();
+  }
+
   void laporan(BookingBloc bloc) {
     isLoading.value = !isLoading.value;
     bloc
@@ -39,14 +52,51 @@ class BookingController extends GetxController {
   }
 
   void booking(BookingBloc bloc) {
-    isLoading.value = !isLoading.value;
-    Map<String, dynamic> data = new Map<String, dynamic>();
-    data['booking_nama'] = namaController.text;
-    data['booking_alamat'] = alamatController.text;
-    data['booking_no_telp'] = noTelpController.text;
-    data['booking_jml_anggota'] = jumlah.value;
-    data['booking_tgl_masuk'] = DateTimeUtil.convert(tglMasuk.value);
-    data['booking_tgl_keluar'] = DateTimeUtil.convert(tglKeluar.value);
-    bloc..add(BookingProsesEvent(data));
+    showDialog(
+        context: Get.context,
+        builder: (context) => AlertDialog(
+              title: Text("Persetujuan"),
+              actions: [
+                TextButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  child: Text("Batal"),
+                ),
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                      isLoading.value = !isLoading.value;
+                      Map<String, dynamic> data = new Map<String, dynamic>();
+                      data['booking_nama'] = namaController.text;
+                      data['booking_alamat'] = alamatController.text;
+                      data['booking_no_telp'] = noTelpController.text;
+                      data['booking_jml_anggota'] = jumlah.value;
+                      data['booking_tgl_masuk'] =
+                          DateTimeUtil.convert(tglMasuk.value);
+                      data['booking_tgl_keluar'] =
+                          DateTimeUtil.convert(tglKeluar.value);
+                      bloc..add(BookingProsesEvent(data));
+                    },
+                    child: Text("Ya, Setuju"))
+              ],
+              content: SingleChildScrollView(
+                child: Html(
+                    data: """${persetujuan.value ?? ""}""",
+                    onImageTap: (String url, RenderContext context,
+                        Map<String, String> attributes, dom.Element element) {
+                      _launchURL(url);
+                      //open image in webview, or launch image in browser, or any other logic here
+                    },
+                    onLinkTap: (String url, RenderContext context,
+                        Map<String, String> attributes, dom.Element element) {
+                      _launchURL(url);
+                    }),
+              ),
+            ));
   }
+
+  void _launchURL(_url) async => await canLaunch(_url)
+      ? await launch(_url)
+      : throw 'Could not launch $_url';
 }
